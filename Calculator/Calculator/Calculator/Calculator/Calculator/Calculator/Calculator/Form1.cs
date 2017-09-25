2560425adb4e29d4a4ddbddb3ea5d37d431dd1c7
Thuhiennguyen <31833651+Thuhiennguyen@users.noreply.github.com>
2017-09-25 23:25:43 +0700

    thu hien
    thu hien

---
+++ Calculator/Calculator/Calculator/Calculator/Calculator/Calculator/Calculator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        bool isTypingNumber = false;
+        enum PhepToan { Cong, Tru, Nhan, Chia};
+        PhepToan pheptoan;
+
+        double nho;
+
+        
+
+        private void NhapSo(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            NhapSo(btn.Text);
+        }
+
+
+        private void NhapSo(string so)
+        {
+            if (isTypingNumber)
+                lblDisplay.Text = lblDisplay.Text + so;
+            else
+            {
+                lblDisplay.Text = so;
+                isTypingNumber = true;
+            }
+        }
+
+        private void NhapPhepToan(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            switch (btn.Text)
+            {
+                case "+": pheptoan = PhepToan.Cong; break;
+                case "-": pheptoan = PhepToan.Tru; break;
+                case "*": pheptoan = PhepToan.Nhan; break;
+                case "/": pheptoan = PhepToan.Chia; break;
+                   
+            }
+            double nho = double.Parse(lblDisplay.Text);
+            isTypingNumber = false;
+        }
+    }
+}
---
+++ Calculator/Calculator/Calculator/Calculator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+        }
+        bool isTypingNumber = false;
+        private void NhapSo(object sender, EventArgs e)
+        {
+            Button btn = (Button) sender;
+            NhapSo(btn.Text);
+        }
+        private void NhapSo(string So)
+        {
+            if (isTypingNumber)
+                lblDisplay.Text = lblDisplay.Text + So;
+            else
+            {
+                lblDisplay.Text = So;
+                isTypingNumber = true;
+            }
+
+        }
+    }
+}
